30940c5a5aaec56dedb7dfd42e6bef6fa8aa30c1
Angélica Coelho <angelicacoelho2501@gmail.com>
2023-06-03 12:12:14 -0300

    Feito alguns ajustes de código e adicionado botão de colar texto

--- index.js
+++ index.js
@@ -2,21 +2,31 @@ const ocultarMensagem = document.getElementById('sem-texto');
 const aparecerBotao = document.getElementById("com-texto");
 const entradaTexto = document.getElementById("entrada-de-texto");
 const textoTratado = document.getElementById("texto-tratado");
+const btnColar = document.querySelector("#btn-colar");
+const btnCriptografar = document.querySelector("#btn-criptografar");
+const btnDescriptografar = document.querySelector("#btn-descriptografar");
 
-function clickCriptografar() {
+btnCriptografar.addEventListener('click', () => {
     checkarTexto();
     criptografar();
-}
+    entradaTexto.value=""; 
+});
 
-function clickDescriptografar() {
+btnDescriptografar.addEventListener('click', () => {
     checkarTexto();
-    descriptografar();
-}
+    descriptografar();  
+    entradaTexto.value="";  
+});
+
+btnColar.addEventListener('click', async (e) => {
+    const colar = await navigator.clipboard.readText();//     
+     entradaTexto.value = colar 
+ });
 
 function btnCopiar(){
     navigator.clipboard.writeText(textoTratado.value).then(()=>{
        alert('Texto copiado!');
-   })
+   });
 }
 
 function ocultarElementos() {
@@ -27,32 +37,34 @@ function ocultarElementos() {
 function checkarTexto(){
     var textoDigitado = entradaTexto.value;
      if (textoDigitado.trim() !== "") {
-         ocultarElementos();
-         
+         ocultarElementos();         
      } else {
-         alert('Ops, você esqueceu de digitar sua mensagem.')
+         alert('Ops, você esqueceu de digitar sua mensagem!')
      }    
  }
 
 function criptografar() {
     var textoDigitado = entradaTexto.value;
-    var saidaDoTexto = document.getElementById("texto-tratado");
+    var saidaDoTexto = document.getElementById("texto-tratado"); 
     var substituicao = textoDigitado.replace(/[áàâãäÁÀÃÂ]/g, "a")
         .replace(/[éèêëÉÈÊ]/g, "e")
         .replace(/[íìîïÍÌÎ]/g, "i")
         .replace(/[óòôõöÓÒÔÕ]/g, "o")
         .replace(/[úùûüÚÙÛÜ]/g, "u")
         .replace(/[çÇ]/g, "c");
-    var textoTratado = substituicao.toLowerCase(); //transformar em sem acento e minusculo 
+    var textoTratado = substituicao.toLowerCase(); 
+    //transformar letras em sem acento e minusculo 
 
     var codificarTexto = textoTratado
         .replace(/[e]/g, "enter")
         .replace(/[o]/g, "ober")
         .replace(/[i]/g, "imes")
         .replace(/[a]/g,"ai")
-        .replace(/[u]/g, "ufat");  //substituindo letas pelos conjuntos      
-    saidaDoTexto.innerHTML += codificarTexto.trim();
-    saidaDoTexto.readOnly = true;
+        .replace(/[u]/g, "ufat");  
+        //substituindo letas pelos conjuntos     
+    saidaDoTexto.innerHTML = ""; // limpar o texto anterior para depois adicionar o novo.
+    saidaDoTexto.innerHTML += codificarTexto.trim();     
+    saidaDoTexto.readOnly = true;    
 
 }
 
@@ -74,7 +86,7 @@ function descriptografar() {
     .replace(/ober/gi, "o")
     .replace(/ufat/gi, "u");  
 
+    saidaDoTexto.innerHTML = "";
     saidaDoTexto.innerHTML += tartarTexto.trim();
     saidaDoTexto.readOnly = true;
 }
-  
